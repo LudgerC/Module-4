db9366df2fb3bc2fc59686d879a3a71e25c81a9f
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-10 10:06:56 +0200

    Create M3.mjs

---
+++ M3.mjs
@@ -0,0 +1,59 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+
+let hoogte = 4;
+
+let startTop = parseFloat(await userInput.question("startTop true of false? "));
+let startLeft = parseFloat(await userInput.question("startLeft true of false? "));
+
+if(startTop == true && startLeft == true)
+{
+    for(let i = 1; i <= hoogte; i++)
+    {
+        for(let j = 1; j <= i; j++)
+        {
+            process.stdout.write("" + i);
+        }
+        
+        console.log(" ");
+    }
+
+}
+
+else if(startTop == false && startLeft == true)
+{
+    for(let i = hoogte; i >= 1; i--)
+    {
+        for(let j = 1; j <= i; j++)
+        {
+            process.stdout.write("" + i);
+        }
+            
+        console.log(" ");
+    }   
+
+}
+
+else if (startTop == true && startLeft == false) 
+{
+    for (let i = 1; i <= hoogte; i++) 
+    {
+            
+        for (let k = 1; k <= hoogte - i; k++) 
+        {
+            process.stdout.write(" ");
+        }
+    
+            
+        for (let j = 1; j <= i; j++) 
+        {
+            process.stdout.write("" + i);
+        }
+    
+        console.log(""); 
+    }
+}
+
+
