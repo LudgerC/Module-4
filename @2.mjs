fde132696cc51af415d2054858ed6191aec5e13b
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-14 10:00:12 +0200

    Update @2.mjs

--- @2.mjs
+++ @2.mjs
@@ -8,20 +8,20 @@ let outline = await userInput.question("Outline? true of false? ") === "true";
 for (let i = 1; i <= hoogte; i++) {
     let rij = '';
 
-    // Voeg spaties toe om de piramide te centreren
+
     for (let j = 1; j <= (hoogte - i); j++) {
         rij += ' ';
     }
 
     if (outline && i > 1 && i < hoogte) {
-        rij += '*';  // Linkerzijde
-        rij += ' '.repeat(2 * i - 3);  // Binnenkant leeg
-        rij += '*';  // Rechterzijde
+        rij += '*';  
+        rij += ' '.repeat(2 * i - 3);  
+        rij += '*';  
     } else {
-        rij += '*'.repeat(2 * i - 1);  // Volledige rij met sterren (bovenste, onderste en gevulde rijen)
+        rij += '*'.repeat(2 * i - 1); 
     }
 
-    console.log(rij);  // Toon de rij
+    console.log(rij); 
 }
 
-userInput.close();  // Sluit de input interface
+userInput.close(); 
